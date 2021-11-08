230393ec9242ebf3443e208b7f6913f1757402f7
cristei <cristei.g772@gmail.com>
2021-11-08 19:43:21 +0200

    createmove

--- src/cavalcade/hooks/functions/base_player.cpp
+++ src/cavalcade/hooks/functions/base_player.cpp
@@ -3,7 +3,14 @@
 bool cavalcade::hooks::base_player::create_move( sdk::cs_player* ecx, unk, f32 input_sample_time, sdk::user_cmd* cmd ) {
 	static auto og = g_mem[ CLIENT_DLL ].get_og< create_move_fn >( HASH_CT( "C_BasePlayer::CreateMove" ) );
 
-	if ( !cmd->m_command_number )
+	// NOTE(para): don't do local checks in features ty
+	// sidenote: when I see pointer checks I want to cry myself to Rust now, honestly
+	if ( !g_ctx.m_local || ( g_ctx.m_local && !g_ctx.m_local->is_alive( ) ) )
+		return og( ecx, input_sample_time, cmd );
+
+	// NOTE(para): IIRC, in mouse shittery, input sample time is an identifier. if we will store any datamap backups, we should prevent our cheat
+	// create-move from running there.
+	if ( !cmd->m_command_number || !input_sample_time )
 		return og( ecx, input_sample_time, cmd );
 
 	if ( og( ecx, input_sample_time, cmd ) )
