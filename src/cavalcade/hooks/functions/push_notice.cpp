149f556204a81710e49e2f89a3b77bd5afe9dd1f
cristei <51248378+cristeigabriel@users.noreply.github.com>
2022-01-14 22:40:20 +0200

    remove todo (not an idea anymore)

--- src/cavalcade/hooks/functions/push_notice.cpp
+++ src/cavalcade/hooks/functions/push_notice.cpp
@@ -4,7 +4,6 @@
 void cavalcade::hooks::push_notice_::push_notice( unk ecx, unk edx, const char* text, int len, const char* normalizer ) {
 	static auto og = g_mem[ CLIENT_DLL ].get_og< push_notice_fn >( HASH_CT( "PushNotice" ) );
 
-	// TODO: skip name (i want this to be done with my original smart caching idea which'll store all this playerinfo detail)
 	// TODO: properly verify if the text is being translated
 	auto stext = std::string_view( text );
 
