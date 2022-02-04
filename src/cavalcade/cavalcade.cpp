e9b97a4e01b3bd1cde9e9ee70e2595129c22e8a1
cristei <51248378+cristeigabriel@users.noreply.github.com>
2022-02-04 17:35:38 +0200

    remove error throwing demo lua

--- src/cavalcade/cavalcade.cpp
+++ src/cavalcade/cavalcade.cpp
@@ -40,9 +40,9 @@ DWORD WINAPI cavalcade::init( unk module_handle ) {
                     state = true
                 end
 
-                a = Vector3.new(1.2, 2.2, 3.3)
-                b = a + a
-                g_ConVars:ConsolePrint('x ' .. b.m_X)
+                --a = Vector3.new(1.2, 2.2, 3.3)
+                --b = a + a
+                --g_ConVars:ConsolePrint('x ' .. b.m_X)
 
                 --g_Render.RectFilled(10, 10, 30, 30, Color.new(255, 0, 255, 255))
                 --g_Render.RectFilled(10, 50, 30, 100, Color.new(0xffff00ff))
